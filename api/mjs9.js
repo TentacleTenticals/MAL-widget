e34208a7527b09d33627bb60f48e77b40be696cc
TentacleTenticals <103366531+TentacleTenticals@users.noreply.github.com>
2025-01-31 16:11:23 +0300

    Update mjs9.js

--- api/mjs9.js
+++ api/mjs9.js
@@ -1,5 +1,6 @@
 export const Mal = {
   url: 'https://api.myanimelist.net/v2',
+  authUrl: 'https://myanimelist.net/v1/oauth2/authorize',
   dataConverter: (o) => {
     if(!o.data) return;
     if(o.method === 'get') return;
@@ -11,6 +12,16 @@ export const Mal = {
       case 'application/x-www-form-urlencoded': return new URLSearchParams(o.data);
       default: return o.data;
     }
+  },
+  cc: function (length){
+    let text = "";
+    const possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-";
+  
+    for(var i = 0; i < length; i++) {
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+  
+    return text;
   },
   wD: function(day, e){
     const days = {
@@ -29,83 +40,100 @@ export const Mal = {
   s: function(o){
     return new URLSearchParams(o);
   },
-    fetch: function(o){
-        return fetch(this.vpn, {
-            method: o.method,
-            headers: {
-                // 'Content-Type': 'application/json',
-                ...o.headers
-            },
-            ...(o.data) && {body: this.dataConverter(o)}
-        }).then(r => r.json().then(
-            res => {
-                console.log('[Fetch]', res);
-                return res;
-            },
-            err => {
-                console.log('[Fetch] ERR', err);
-                console.log('R', r);
-            }
-        ))
-    },
-    list: function(o){
-        o.url = 'https://api.myanimelist.net/v2/anime/17074/my_list_status';
+  fetch: function(o){
+      return fetch(this.vpn, {
+          method: o.method,
+          headers: {
+              ...o.headers
+          },
+          ...(o.data) && {body: this.dataConverter(o)}
+      }).then(r => r.json().then(
+          res => {
+              // console.log('[MAL]', res);
+              return res;
+          },
+          err => {
+              console.log('[MAL] ERR', err);
+              console.log('[MAL] R', r);
+          }
+      ))
+  },
+  getToken: function(o){
+    o.url = 'https://myanimelist.net/v1/oauth2/token';
+    o.method = 'POST';
 
-        this.fetch(o);
-    },
-    status: function(o){
-      o.method = 'get';
-      o.url = this.vpn;
-      o.query = new URLSearchParams({
-        status: 'watching',
-        limit: 4
-      });
-      o.headers = {
-        Head: toString({
-          'Authorization': 'Bearer '+this.token
-        }),
-        Info: toString({
-          url: 'https://api.myanimelist.net/v2/users/@me/animelist?'+o.query
-        })}
+    o.headers = {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      Url: `${o.url}?${o.query && this.s(o.query)||''}`
+    };
 
-      console.log('H', o.headers)
+    return this.fetch(o);
+  },
+  updToken: function(o){
+    o.url = 'https://myanimelist.net/v1/oauth2/token';
+    o.method = 'POST';
 
-      return this.fetch(o);
-    },
-    search: function(o){
-      o.url = this.vpn;
-      o.method = 'GET';
+    o.headers = {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Authorization': 'Bearer '+this.token,
+      Url: `${o.url}?${o.query && this.s(o.query)||''}`
+    };
 
-      // console.log('QQQ', o);
+    return this.fetch(o);
+  },
+  list: function(o){
+      o.url = 'https://api.myanimelist.net/v2/anime/17074/my_list_status';
 
-      o.headers = {
-        'Authorization': 'Bearer '+this.token,
-        Url: `https://api.myanimelist.net/v2/anime?${o.query && this.s(o.query)||''}`
-      }
-      return this.fetch(o);
-    },
-    getList: function(o){
-      o.url = this.vpn;
-      o.method = 'GET';
+      this.fetch(o);
+  },
+  status: function(o){
+    o.method = 'get';
+    o.url = this.vpn;
+    o.query = new URLSearchParams({
+      status: 'watching',
+      limit: 4
+    });
+    o.headers = {
+      Head: toString({
+        'Authorization': 'Bearer '+this.token
+      }),
+      Info: toString({
+        url: 'https://api.myanimelist.net/v2/users/@me/animelist?'+o.query
+      })}
 
-      o.headers = {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer '+this.token,
-        Url: `${this.url}/${o.type}/${o.value||''}?${o.query && this.s(o.query)||''}`
-      }
+    console.log('H', o.headers)
 
-      return this.fetch(o);
-    },
-    updateList: function(o){
-      o.url = this.vpn;
-      o.method = 'PUT';
+    return this.fetch(o);
+  },
+  search: function(o){
+    o.method = 'GET';
 
-      o.headers = {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'Authorization': 'Bearer '+this.token,
-        Url: `${this.url}/${o.type}/${o.value||''}/my_list_status?${o.query && this.s(o.query)||''}`
-      }
-
-      return this.fetch(o);
+    o.headers = {
+      'Authorization': 'Bearer '+this.token,
+      Url: `${this.url}?${o.query && this.s(o.query)||''}`
     }
+    return this.fetch(o);
+  },
+  getList: function(o){
+    o.method = 'GET';
+
+    o.headers = {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer '+this.token,
+      Url: `${this.url}/${o.type}/${o.value||''}?${o.query && this.s(o.query)||''}`
+    }
+
+    return this.fetch(o);
+  },
+  updateList: function(o){
+    o.method = 'PUT';
+
+    o.headers = {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Authorization': 'Bearer '+this.token,
+      Url: `${this.url}/${o.type}/${o.value||''}/my_list_status?${o.query && this.s(o.query)||''}`
+    }
+
+    return this.fetch(o);
+  }
 };
