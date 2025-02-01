77ac9f960577c70fb7b4fb32202e70944b299f39
TentacleTenticals <103366531+TentacleTenticals@users.noreply.github.com>
2025-02-01 11:07:39 +0300

    Update and rename connect4.js to connect5.js

--- main/connect5.js
+++ main/connect5.js
@@ -1,4 +1,4 @@
-export function connect(Mal, s, title){
+export function connect(Mal, o){
   const filter = /(?<a>[.,:;]+)|(?![\w]+)(?<b>-)(?![\w]+)|(?<c>-)/gm;
   const fixer = (text) => text.replace(filter, (_, a, b, c) => {
     if(a){
@@ -15,9 +15,9 @@ export function connect(Mal, s, title){
     }
   }).toLowerCase();
   Mal.search({
-    type: 'anime',
+    type: o.type,
     query: {
-      q: title.slice(0, 64),
+      q: o.title.slice(0, 64),
       // offset: 1,
       limit: 20,
       nsfw: true
@@ -26,14 +26,14 @@ export function connect(Mal, s, title){
     res => {
       console.log('[MAL API]', res);
       res.data.forEach(r => {
-        console.log(r.node.id);
-        if(fixer(r.node.title) === title){
+        // console.log(r.node.id);
+        if(fixer(r.node.title) === o.title){
           console.log('GOT one!!!', {id: r.node.id, title:r.node.title});
-          s.main.id = r.node.id;
-          s.main.title = r.node.title;
+          o.s.main.id = r.node.id;
+          o.s.main.title = r.node.title;
   
           Mal.getList({
-                value: s.main.id,
+                value: o.s.main.id,
                 type: 'anime',
                 query: {
                   fields: ['id', 'title', 'rank', 'rating', 'popularity', 'score', 'mean', 'status', 'broadcast', 'statistics', 'start_date', 'my_list_status', 'num_episodes']
@@ -42,17 +42,17 @@ export function connect(Mal, s, title){
                 l => {
                   console.log('LIST', l);
                   console.log('S', l.my_list_status?.score||0)
-                  s.main.rating = l.mean;
-                  s.main.rank = l.rank;
-                  s.main.status = l.status;
-                  s.main.weekDay = l.broadcast.day_of_the_week;
-                  s.main.weekTime = l.broadcast.start_time;
-                  s.main.url = `https://myanimelist.net/anime/${r.node.id}`;
+                  o.s.main.rating = l.mean;
+                  o.s.main.rank = l.rank;
+                  o.s.main.status = l.status;
+                  o.s.main.weekDay = l.broadcast.day_of_the_week;
+                  o.s.main.weekTime = l.broadcast.start_time;
+                  o.s.main.url = `https://myanimelist.net/anime/${r.node.id}`;
   
-                  s.me.status = l.my_list_status?.status;
-                  s.me.rating = l.my_list_status?.score||0;
-                  s.main.epsNum = l.num_episodes;
-                  s.me.eps = l.my_list_status?.num_episodes_watched||0;
+                  o.s.me.status = l.my_list_status?.status;
+                  o.s.me.rating = l.my_list_status?.score||0;
+                  o.s.main.epsNum = l.num_episodes;
+                  o.s.me.eps = l.my_list_status?.num_episodes_watched||0;
                 }
               )
         }
