84aa649dfdd6405d9f752a24eb4f518e6008932b
TentacleTenticals <103366531+TentacleTenticals@users.noreply.github.com>
2025-02-11 15:13:49 +0300

    Update tokenModal.js

--- interface/tokenModal.js
+++ interface/tokenModal.js
@@ -103,11 +103,11 @@ export function tokenModal(El, Mal, o){
                     //   d.close();
                     // };
                     d.close();
-                    return o.promise[0]({tokenInfo:'tokens created', ...res});
+                    return o.promise[0]({tokenInfo:{msg:'tokens created'}, ...res});
                   },
                   err => {
                     console.log('[MAL Widget ERR] Токены не получены!', err);
-                    return o.promise[1]({tokenInfo:'error', err:err});
+                    return o.promise[1]({tokenInfo:{msg:'error'}, err:err});
                   }
                 );
               }
