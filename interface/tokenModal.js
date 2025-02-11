9ba6ad8b60487010ea05a93bc880533a1ddc2aa4
TentacleTenticals <103366531+TentacleTenticals@users.noreply.github.com>
2025-02-11 13:32:58 +0300

    Update tokenModal.js

--- interface/tokenModal.js
+++ interface/tokenModal.js
@@ -87,7 +87,7 @@ export function tokenModal(El, Mal, o){
               onclick: () => {
 
                 return Mal.getToken({
-                  url: o.url,
+                  url: o.secrets.catcher_url,
                   ...o.secrets,
                   codeVer: chall,
                   // code: o.secrets.code
