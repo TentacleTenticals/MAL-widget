6272217bb8949d175edde89b65b64e0f9d06a8c1
TentacleTenticals <103366531+TentacleTenticals@users.noreply.github.com>
2025-02-18 14:55:24 +0300

    Update base.js

--- css/base.js
+++ css/base.js
@@ -15,4 +15,23 @@ export const baseCSS = () => `
     display: inline;
   }
 }
+
+.scrol {
+  &::-webkit-scrollbar-corner {
+    background-color: unset;
+  }
+
+  &.lite::-webkit-scrollbar {
+    width: 2px;
+    height: 2px;
+  }
+  &.mid::-webkit-scrollbar {
+    width: 4px;
+    height: 4px;
+  }
+  &.big::-webkit-scrollbar {
+    width: 6px;
+    height: 6px;
+  }
+}
 `
